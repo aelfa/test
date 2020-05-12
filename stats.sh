5a4790bbf8abfc998a77628113ba5ea927dd1076
doob187 <60312740+doob187@users.noreply.github.com>
2020-05-12 21:55:38 +0200

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -10,32 +10,34 @@ inx1=$(($in + $i))
 out=`cat /tmp/monitor | grep tx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
 outx1=$(($out + $i))
 
+echo " ~~~~~~~~~~~~~~~~~~~~"
 echo "INCOMING : ${inx1}" 
 echo "OUTGOING : ${outx1}"
- 
+echo " ~~~~~~~~~~~~~~~~~~~~" 
+
 ##### Second Test
 sleep 30
 vnstat -tr > /tmp/monitor2
-ini2=`cat /tmp/monitor2 | grep rx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
+in2=`cat /tmp/monitor2 | grep rx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
 inx2=$(($in2 + $i))
 out2=`cat /tmp/monitor2 | grep tx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
 outx2=$(($out2 + $i))
 
 echo "INCOMING : ${inx2}" 
 echo "OUTGOING : ${outx2}"
+echo " ~~~~~~~~~~~~~~~~~~~~"
 
 #### Third Test
 sleep 30
 vnstat -tr > /tmp/monitor3
-ini3=`cat /tmp/monitor3 | grep rx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
-inx3=$(($in2 + $i))
+in3=`cat /tmp/monitor3 | grep rx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
+inx3=$(($in3 + $i))
 out3=`cat /tmp/monitor3 | grep tx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
 outx3=$(($out3 + $i))
 
 echo "INCOMING : ${inx3}" 
 echo "OUTGOING : ${outx3}"
-
-done
+echo " ~~~~~~~~~~~~~~~~~~~~"
 
 echo " TEST 1 "
 echo "INCOMING : ${inx1}" 
@@ -47,3 +49,5 @@ echo " TEST 3 "
 echo "INCOMING : ${inx3}" 
 echo "OUTGOING : ${outx3}"
 echo " 3 TESTS DONE " 
+echo " ~~~~~~~~~~~~~~~~~~~~"
+done
