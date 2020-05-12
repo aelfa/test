b6b79c2730e5a05e90f316e19894b2e7ee636492
doob187 <60312740+doob187@users.noreply.github.com>
2020-05-12 21:29:42 +0200

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 while true; do
-#Monitory Bandwidth Developed by Adminlogs.info
-#Tested and verified with CentOS and RedHat 5
 hostname=`hostname`
+
 vnstat -tr > /tmp/monitor
 i=0
 in=`cat /tmp/monitor | grep rx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
@@ -11,10 +10,10 @@ inx=$(($in + $i))
 out=`cat /tmp/monitor | grep tx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
 outx=$(($out + $i))
 
-echo "$inx" 
-echo "$outx"
+echo "INCOMING : ${inx1}" 
+echo "OUTGOING : ${outx1}"
  
-##### Second Test after 2 minutes
+##### Second Test
 sleep 30
 vnstat -tr > /tmp/monitor2
 ini2=`cat /tmp/monitor2 | grep rx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
@@ -22,10 +21,10 @@ inx2=$(($in2 + $i))
 out2=`cat /tmp/monitor2 | grep tx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
 outx2=$(($out2 + $i))
 
-echo "$inx2" 
-echo "$ourx2"
+echo "INCOMING : ${inx2}" 
+echo "OUTGOING : ${outx2}"
 
-#### Third Test after 4 minutes
+#### Third Test
 sleep 30
 vnstat -tr > /tmp/monitor3
 ini3=`cat /tmp/monitor3 | grep rx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
@@ -33,7 +32,18 @@ inx3=$(($in2 + $i))
 out3=`cat /tmp/monitor3 | grep tx | grep -v kbit | awk '{print $2}' | cut  -d . -f1`
 outx3=$(($out3 + $i))
 
-echo "$inx3"
-echo "$outx3"
+echo "INCOMING : ${inx3}" 
+echo "OUTGOING : ${outx3}"
 
 done
+
+echo " TEST 1 "
+echo "INCOMING : ${inx1}" 
+echo "OUTGOING : ${outx1}"
+echo " TEST 2 "
+echo "INCOMING : ${inx2}" 
+echo "OUTGOING : ${outx2}"
+echo " TEST 3 "
+echo "INCOMING : ${inx3}" 
+echo "OUTGOING : ${outx3}"
+echo " 3 TESTS DONE " 
