fc793ddb6ce94e05f3f3693cd146b60069769803
doob187 <60312740+doob187@users.noreply.github.com>
2020-07-09 19:21:59 +0200

    Update radarrbash.sh

--- radarrbash.sh
+++ radarrbash.sh
@@ -3,11 +3,7 @@ list_all_folders() {
 tree -d -L 1 /mnt/downloads/torrent/FILME-HD | awk '{print $2}' | tail -n +2 | head -n -2 >/tmp/radarradd
 }
 
-#{
-#while read p; do
-#/usr/bin/curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "X-Api-Key: $apiKey" -X POST -d '{"title":"'"$title"'","publishDate":"'"$date"'"}' http://radarr:7878/api/release/push
-#done </tmp/radarradd
-#}
+
 radarr_add() {
 info=$(cat /opt/appdata/radarr/config.xml)
 info=${info#*<ApiKey>} 1>/dev/null 2>&1
@@ -16,13 +12,13 @@ echo $apiKeycut
 downloadUrl=folder
 date=$(date -u +"%Y-%m-%d %H:%M:%SZ")
 
- for radarradd in `cat /tmp/radarradd` ; do
+basefolder="/mnt/downloads/torrent/FILME-HD"
+cd ${basefolder}
+ for radarradd in `find . -maxdepth 1 -type d | grep -v "^\.$" `; do
      /usr/bin/curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "X-Api-Key: ${apiKeycut}" -X POST -d '{"title":"'"$radarradd"'","downloadUrl":"'"$downloadUrl"'","downloadProtocol":"torrent","publishDate":"'"$date"'"}'' http://radarr:7878/api/release/push
     echo "Movie Add : ${radarradd} successfull"
  done
 
 }
-
-list_all_folders
 
 radarr_add
