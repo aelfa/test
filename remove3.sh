372a96d763bad93fca77925f04feaec88aa88819
doob187 <60312740+doob187@users.noreply.github.com>
2020-07-15 19:51:21 +0200

    Update remove3.sh

--- remove3.sh
+++ remove3.sh
@@ -10,76 +10,76 @@ FIND_DEL_EMPTY='--rmdirs'
 CONFIG='--config'
 CONFIG_FILE='/opt/appdata/plexguide/rclone.conf'
 
-UNWANTED_FILES='
-**.nfo
-**.jpeg
-**.jpg
-**.gif
-**.rar
-**sample**
-**.1
-**.2
-**.3
-**.4
-**.5
-**.6
-**.7
-**.8
-**.9
-**.10
-**.11
-**.12
-**.13
-**.14
-**.15
-**.16
-**.html~
-**.url
-**.htm
-**.html
-**.sfv
-**sample.*
-**.sh
-**.pdf
-**.doc
-**.docx
-**.xls
-**.xlsx
-**.xml
-**.html
-**.htm
-**.exe
-**.lsn
-**.nzb
-Click.rar
-What.rar
-**sample*
-**SAMPLE*
-**SaMpLE*
-**.nfo
-**.jpeg
-**.jpg
-**.srt
-**.idx
-**.rar
-**sample**
-**.png**
-'
+UNWANTED_FILES=(
+    '*.nfo'
+    '*.jpeg'
+    '*.jpg'
+    '*.gif'
+    '*.rar'
+    '*sample*'
+    '*.1'
+    '*.2'
+    '*.3'
+    '*.4'
+    '*.5'
+    '*.6'
+    '*.7'
+    '*.8'
+    '*.9'
+    '*.10'
+    '*.11'
+    '*.12'
+    '*.13'
+    '*.14'
+    '*.15'
+    '*.16'
+    '*.html~'
+    '*.url'
+    '*.htm'
+    '*.html'
+    '*.sfv'
+    '*sample.*'
+    '*.sh'
+    '*.pdf'
+    '*.doc'
+    '*.docx'
+    '*.xls'
+    '*.xlsx'
+    '*.xml'
+    '*.html'
+    '*.htm'
+    '*.exe'
+    '*.lsn'
+    '*.nzb'
+    'Click.rar'
+    'What.rar'
+    '*sample*'
+    '*SAMPLE*'
+    '*SaMpLE*'
+    '*.nfo'
+    '*.jpeg'
+    '*.jpg'
+    '*.srt'
+    '*.idx'
+    '*.rar'
+    '*sample*'
+    '*.png*'
+)
 
 #if grep -q gcrypt ${CONFIG_FILE}; then
-#   ${FIND} ${FIND_DEL_NAME} gcrypt:/ ${FIND_ADD_NAME}=${UNWANTED_FILES} ${CONFIG}=${CONFIG_FILE}
+#   ${FIND} ${FIND_DEL_NAME} gcrypt: ${FIND_ADD_NAME}=${UNWANTED_FILES} ${CONFIG}=${CONFIG_FILE}
 #fi
 
 #if grep -q gdrive ${CONFIG_FILE}; then
-#   ${FIND} ${FIND_DEL_NAME} gdrive:/ ${FIND_ADD_NAME}=${UNWANTED_FILES} ${CONFIG}=${CONFIG_FILE}
+#   ${FIND} ${FIND_DEL_NAME} gdrive: ${FIND_ADD_NAME}=${UNWANTED_FILES} ${CONFIG}=${CONFIG_FILE}
 #fi
 
 #if grep -q tcrypt ${CONFIG_FILE}; then
-#   ${FIND} ${FIND_DEL_NAME} tcrypt:/ ${FIND_ADD_NAME}=${UNWANTED_FILES} ${CONFIG}=${CONFIG_FILE}
+#   ${FIND} ${FIND_DEL_NAME} tcrypt: ${FIND_ADD_NAME}=${UNWANTED_FILES} ${CONFIG}=${CONFIG_FILE}
 #fi
 
 #if grep -q tdrive ${CONFIG_FILE}; then
-#   ${FIND} ${FIND_DEL_NAME} tdrive:/ ${FIND_ADD_NAME}=${UNWANTED_FILES} ${CONFIG}=${CONFIG_FILE}
+#   ${FIND} ${FIND_DEL_NAME} tdrive: ${FIND_ADD_NAME}=${UNWANTED_FILES} ${CONFIG}=${CONFIG_FILE}
 #fi
 
-${FIND} --dry-run ${FIND_DEL_NAME} gcrypt:/ ${FIND_ADD_NAME}=${UNWANTED_FILES} ${CONFIG}=${CONFIG_FILE}
+${FIND} --dry-run ${FIND_DEL_NAME} gcrypt: ${FIND_ADD_NAME}=${UNWANTED_FILES} ${CONFIG}=${CONFIG_FILE}
