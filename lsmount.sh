c7bc631695aec5d326b56f5ecefc8bef694cd084
doob187 <60312740+doob187@users.noreply.github.com>
2020-08-24 23:26:39 +0200

    Update lsmount.sh

--- lsmount.sh
+++ lsmount.sh
@@ -10,7 +10,7 @@ IFS=$'\n'
 filter="$1"
 config=/config/rclone.conf
 #rclone listremotes | gawk "$filter"
-mapfile -t mounts < <(eval rclone listremotes --config=${config} | grep "$filter" | sed -e 's/[pgunion:]//g' | sed -e 's/[GDSA00-99C:]//g' | sed '/^$/d')
+mapfile -t mounts < <(eval rclone listremotes --config=${config} | grep "$filter" | sed -e 's/[GDSA00-99C:]//g' | sed '/^$/d')
 ## function source end
 
 DIR_CACHE_TIME=${DIR_CACHE_TIME:-2m}
@@ -28,51 +28,23 @@ POLL_INTERVAL=${POLL_INTERVAL:-5m}
 LOGLEVEL=${LOGLEVEL:-INFO}
 UAGENT=${UAGENT:-somerandstring}
 
-
-# These flags are applied to all sets. Tweak them as you like
-FLAGS="
-  --config=${config}
-  --log-file=/logs/drive/rclone-$i.log
-  --log-level=${LOFLEVEL}
-  --uid=1000 --gid=1000 --umask=002
-  --allow-other
-  --timeout=1h
-  --tpslimit=8
-  --drive-skip-gdocs
-  --user-agent="${UAGENT}"
-  --dir-cache-time="${DIR_CACHE_TIME}"
-  --vfs-cache-mode="${VFS_CACHE_MODE}"
-  --vfs-cache-max-age=${VFS_CACHE_MAX_AGE}
-  --vfs-cache-max-size=${VFS_CACHE_MAX_SIZE}
-  --vfs-read-chunk-size-limit=${VFS_READ_CHUNK_SIZE_LIMIT}
-  --vfs-read-chunk-size=${VFS_READ_CHUNK_SIZE}
-  --buffer-size=${BUFFER_SIZE}
-  --fast-list
-  --tpslimit-burst=50
-  --stats=10s
-  --max-backlog=2000000
-  --ignore-case
-  --no-update-modtime
-  --drive-chunk-size=128M
-  --drive-use-trash=false
-  --track-renames
-  --use-mmap
-  --drive-server-side-across-configs=true
-  --drive-stop-on-upload-limit
-  "
+## RUN MOUNT ##
 
 for i in ${mounts[@]}; do
   echo; echo CREATE EMPTY DIRECTORIES for $i; echo
   mkdir -p /mnt/$i
-  #if [ size command here for check ]; then  when failes.
-  #   echo; echo CHECK FOR EMPTY MOUNTS & DIRECTORIES for $i; echo
-  #   /bin/fusermount -uz /mnt/$i > /dev/null
-  #fi
-  echo; echo STARTING MOUNT of from $i; echo
+  if [[ $(ls -a /mnt/$i | wc -l)" -ne 0 ]]; then
+     echo; echo unmounting for $i drive; echo
+    /usr/bin/fusermount -uzq /mnt/$i > /dev/null
+  fi
+  echo; echo create logfolder for $i; echo
+  mkdir -p /logs/$i/rclone-$i.log
+  touch /logs/$i/rclone-$i.log
+  echo; echo STARTING MOUNT for $i; echo
   /usr/bin/rclone mount $i: /mnt/$i \
          --config="${config}" \
-         --log-file=/logs/drive/rclone-$i.log \
-         --log-level="${LOFLEVEL}" \
+         --log-file=/logs/$i/rclone-$i.log \
+         --log-level="${LOGLEVEL}" \
          --uid=1000 --gid=1000 --umask=002 \
          --allow-other --timeout=1h --tpslimit=8 \
          --drive-skip-gdocs --user-agent="${UAGENT}" \
