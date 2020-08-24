d377feaf88da9694405ebe3409128e016c92c67c
doob187 <60312740+doob187@users.noreply.github.com>
2020-08-24 23:04:28 +0200

    Update lsmount.sh

--- lsmount.sh
+++ lsmount.sh
@@ -82,7 +82,7 @@ for i in ${mounts[@]}; do
          --vfs-cache-max-size="${VFS_CACHE_MAX_SIZE}" \
          --vfs-read-chunk-size-limit="${VFS_READ_CHUNK_SIZE_LIMIT}" \
          --vfs-read-chunk-size="${VFS_READ_CHUNK_SIZE}" \
-         --buffer-size=${BUFFER_SIZE} --fast-list \
+         --buffer-size="${BUFFER_SIZE}" --fast-list \
          --tpslimit-burst=50 --stats=10s \
          --drive-chunk-size=128M --drive-use-trash=false \
          --drive-server-side-across-configs=true \
