26926021d579d6fb9a00636d7f98bc2ef171419b
doob187 <60312740+doob187@users.noreply.github.com>
2020-11-23 11:53:01 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,65 +1,81 @@
 #!/bin/bash
-
-# while-menu-dialog: a menu driven system information program
-
-DIALOG_CANCEL=1
-DIALOG_ESC=255
-HEIGHT=0
-WIDTH=0
-
 display_result() {
   dialog --title "$1" \
     --no-collapse \
     --msgbox "$result" 0 0
 }
+display_user() {
+  dialog --title "$1" \
+  --inputbox 16 51 2> $tmp_file_user
+}
+display_password() {
+  dialog --title "$1" \
+   --no-collapse \
+   --passwordbox 16 51 2> $tmp_file_password
+}
+create_user() {
+  tmp_file_user=$(tempfile 2>/dev/null) || tmp_file_user=/tmp/test$$
+  tmp_file_password=$(tempfile 2>/dev/null) || tmp_file_password=/tmp/test$$
+  username=$(cat $tmp_file_user)
+  pass=$(cat $tmp_file_password)
+  pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+  useradd -m -p $pass $username
+  usermod -aG sudo $username
+  sudo usermod -s /bin/bash $username
+  usermod -aG video $username
+  usermod -aG docker $username
+  trap "rm -f $tmp_file_user" 0 1 2 5 15
+  trap "rm -f $tmp_file_password" 0 1 2 5 15
+}
 
+display_exist() {
+    dialog --title "$1" \
+    --no-collapse \
+    --msgbox "$result" 0 0
+} 
 while true; do
   exec 3>&1
   selection=$(dialog \
-    --backtitle "System Information" \
+    --backtitle "Sudobox.Io Install Interface" \
     --title "Menu" \
     --clear \
     --cancel-label "Exit" \
     --menu "Please select:" $HEIGHT $WIDTH 4 \
-    "1" "Display System Information" \
-    "2" "Display Disk Space" \
-    "3" "Display Home Space Utilization" \
+    "1" "Create a sudo user" \
+    "2" "Use existing sudo user" \
+    "3" "Install Sudobox.io now" \
     2>&1 1>&3)
   exit_status=$?
   exec 3>&-
   case $exit_status in
     $DIALOG_CANCEL)
-      clear
-      echo "Program terminated."
-      exit
-      ;;
+      clear && echo "Program terminated." 
+      exit ;;
     $DIALOG_ESC)
-      clear
-      echo "Program aborted." >&2
+      clear && echo "Program aborted." >&2
       exit 1
       ;;
   esac
   case $selection in
     0 )
-      clear
-      echo "Program terminated."
+      clear && echo "Program terminated."
       ;;
     1 )
-      result=$(echo "Hostname: $HOSTNAME"; uptime)
-      display_result "System Information"
+      display_user "Create sudo user"
+      display_password "Typ password" 
+      create_user && clear
       ;;
     2 )
-      result=$(df -h)
-      display_result "Disk Space"
+      display_exist "Use existing sudo User"
+      usermod -aG sudo $(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')
+      sudo usermod -s /bin/bash $(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')
+      sudo usermod -aG video $(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')
+      sudo usermod -aG docker $(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')
+      result=$(echo "All done)
       ;;
     3 )
-      if [[ $(id -u) -eq 0 ]]; then
-        result=$(du -sh /home/* 2> /dev/null)
-        display_result "Home Space Utilization (All Users)"
-      else
-        result=$(du -sh $HOME 2> /dev/null)
-        display_result "Home Space Utilization ($USER)"
-      fi
+      ## run the rest after here 
+      clear
       ;;
   esac
 done
