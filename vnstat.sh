50f147dc6dd42e5b2a44c6d705065d185a490ef1
doob187 <60312740+doob187@users.noreply.github.com>
2020-12-14 20:01:40 +0100

    Update vnstat.sh

--- vnstat.sh
+++ vnstat.sh
@@ -10,7 +10,7 @@ sed -i '/UnitMode/s/0/1/' /etc/vnstat.conf
 sed -i 's/Locale "-"/Locale "LC_ALL=en_US.UTF-8"/g' /etc/vnstat.conf
 /etc/init.d/vnstat restart 2>&1 >>/dev/null
 
-echo "done'
+echo "done"
 sudo docker restart uploader
 
 sleep 1
