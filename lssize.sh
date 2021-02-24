50860e376594b99e95e924bf104266a497ff1bb8
doob187 <60312740+doob187@users.noreply.github.com>
2021-02-24 21:26:45 +0100

    Update lssize.sh

--- lssize.sh
+++ lssize.sh
@@ -10,12 +10,26 @@ IFS=$'\n'
 filter=$1
 ## function source end
 
-config=/opt/appdata/mount/config/rclone-docker.conf
+config=/opt/appdata/mount/rclone.conf
 mapfile -t mount < <(eval rclone listremotes --config=${config} | grep "$filter" | sed '/GDSA/d' | sed '/pgunion/d')
 ## function source end
 
 for i in ${mounts[@]}; do
   echo; echo For $i | tee -a mount.sizes
-  rclone size $i: --config=${config} --fast-list | tee -a /home/mount.sizes
+  rclone size $i --config=${config} --fast-list | tee -a /home/mount.sizes
 done
+rcc=/home/mount.sizes
+if [[ -f "$rcc" ]]; then
+endline=$(cat /home/mount.sizes)
+tee <<-EOF
 
+━━━━━━━━━━━━━━━━━━━━━━━━
+     💪  Rclone size check done
+━━━━━━━━━━━━━━━━━━━━━━━━
+
+ $endline
+
+━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+  read -rp '↘️ Press [ENTER] to quit ' typed </dev/tty
+fi
