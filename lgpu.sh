c49a1ca1f64536332eb74eb52551bfd785539296
doob187 <60312740+doob187@users.noreply.github.com>
2021-03-12 16:31:10 +0100

    Update lgpu.sh

--- lgpu.sh
+++ lgpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- gpu="i915 nvidia"
- for i in ${gpu}; do
-   lshw -C video | grep -qE '$i' && echo true || echo false
- done
+gpu="i915 nvidia"
+for i in ${gpu}; do
+   lshw -C video | grep -qE $i && echo true || echo false 
+done
