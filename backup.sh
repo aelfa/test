8f31fcafcd1bd136be2c8d9fbc8b5ec6271cdb67
doob187 <60312740+doob187@users.noreply.github.com>
2021-04-02 21:59:01 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,13 +5,13 @@
 # shellcheck disable=SC2086
 # shellcheck disable=SC2006
 
-$(command -v docker) system prune -af
+$(command -v docker) system prune -af 1>/dev/null 2>&1
+$(command -v docker) pull ghcr.io/doob187/docker-remote:latest 1>/dev/null 2>&1
 
 dockers=$(docker ps -aq --format '{{.Names}}' | sed '/^$/d')
 for i in ${dockers};do
     $(command -v docker) run --rm -v /opt/appdata:/backup/$i -v /mnt:/mnt ghcr.io/doob187/docker-remote:latest backup $i
 done
 
-$(command -v docker) system prune -af
-
+$(command -v docker) system prune -af 1>/dev/null 2>&1
 exit
