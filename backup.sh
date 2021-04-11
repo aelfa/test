c015e1b81ea1c7e4b8ea84692d3b7e1f7085d122
doob187 <60312740+doob187@users.noreply.github.com>
2021-04-11 11:24:03 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,9 +7,11 @@
 
 $(command -v docker) system prune -af 1>/dev/null 2>&1
 $(command -v docker) pull ghcr.io/doob187/docker-remote:latest 1>/dev/null 2>&1
+$(command -v docker) pull red5d/docker-autocompose 1>/dev/null 2>&1
 
 dockers=$(docker ps -aq --format '{{.Names}}' | sed '/^$/d' | grep -v 'trae' | grep -v 'auth')
 for i in ${dockers};do
+    $(command -v docker) run --rm -v /var/run/docker.sock:/var/run/docker.sock red5d/docker-autocompose $docker > /opt/appdata/$i/docker-compose.yml
     $(command -v docker) run --rm -v /opt/appdata:/backup/$i -v /mnt:/mnt ghcr.io/doob187/docker-remote:latest backup $i
 done
 
