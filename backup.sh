d9c5bac9a5fc3d07d39b862ad6aa791dfc33c32e
doob187 <60312740+doob187@users.noreply.github.com>
2021-04-25 17:43:39 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -16,12 +16,11 @@
 # shellcheck disable=SC2006
 
 $(command -v docker) system prune -af 1>/dev/null 2>&1
-$(command -v docker) pull ghcr.io/doob187/docker-remote:nightly 1>/dev/null 2>&1
+$(command -v docker) pull ghcr.io/doob187/docker-remote:latest 1>/dev/null 2>&1
 
 dockers=$(docker ps -aq --format '{{.Names}}' | sed '/^$/d' | grep -v 'trae' | grep -v 'auth')
 for i in ${dockers};do
-    if [[ -f "/opt/appdata/$i/docker-compose.yml" ]];then $(command -v rm) -rf /opt/appdata/$i/docker-compose.yml 1>/dev/null 2>&1;fi
-    $(command -v docker) run --rm -v /opt/appdata:/backup/$i -v /mnt:/mnt ghcr.io/doob187/docker-remote:nightly backup $i local
+    $(command -v docker) run --rm -v /opt/appdata:/backup/$i -v /mnt:/mnt ghcr.io/doob187/docker-remote:latest backup $i local
     $(command -v rm) -rf /opt/appdata/$i/docker-compose.yml 1>/dev/null 2>&1
 done
 
